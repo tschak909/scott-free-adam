b62d0e618b3981bd0d1c6b55e92f31dbc99008c4
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2022-01-31 19:42:55 -0600

    fix dist.

--- dist.sh
+++ dist.sh
@@ -18,7 +18,7 @@ cpmcp -f adam scott-adams-adventures-disk1.dsk dist1/adv4.dat 0:adv4.dat
 cpmcp -f adam scott-adams-adventures-disk1.dsk dist1/adv5.dat 0:adv5.dat
 cpmcp -f adam scott-adams-adventures-disk1.dsk dist1/games.txt 0:games.txt
 cpmcp -f adam scott-adams-adventures-disk2.dsk dist2/adventur.com 0:adventur.com
-cpmcp -f adam scott-adams-adventures-disk2.dsk dist1/adv6.dat 0:adv6.dat
+cpmcp -f adam scott-adams-adventures-disk2.dsk dist2/adv6.dat 0:adv6.dat
 cpmcp -f adam scott-adams-adventures-disk2.dsk dist2/adv7.dat 0:adv7.dat
 cpmcp -f adam scott-adams-adventures-disk2.dsk dist2/adv8.dat 0:adv8.dat
 cpmcp -f adam scott-adams-adventures-disk2.dsk dist2/adv9.dat 0:adv9.dat
@@ -31,7 +31,7 @@ cpmcp -f adam scott-adams-adventures-disk3.dsk dist3/adv13.dat 0:adv13.dat
 cpmcp -f adam scott-adams-adventures-disk3.dsk dist3/adv14.dat 0:adv14.dat
 cpmcp -f adam scott-adams-adventures-disk3.dsk dist3/adv15.dat 0:adv15.dat
 cpmcp -f adam scott-adams-adventures-disk3.dsk dist3/games.txt 0:games.txt
-cpmcp -f adam scott-adams-adventures-disk3.dsk dist4/adventur.com 0:adventur.com
+cpmcp -f adam scott-adams-adventures-disk4.dsk dist4/adventur.com 0:adventur.com
 cpmcp -f adam scott-adams-adventures-disk4.dsk dist4/adv16.dat 0:adv16.dat
 cpmcp -f adam scott-adams-adventures-disk4.dsk dist4/adv17.dat 0:adv17.dat
 cpmcp -f adam scott-adams-adventures-disk4.dsk dist4/games.txt 0:games.txt
